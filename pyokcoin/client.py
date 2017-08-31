417cf053159bac51211ed3ec7d6d6c5e74372062
chiangqiqi <woshiqiqiye@gmail.com>
2017-08-31 13:31:54 +0800

    add trade api implementation

--- pyokcoin/client.py
+++ pyokcoin/client.py
@@ -26,17 +26,26 @@ class OKCoinReq:
     def _get_path(self, subpath):
         return self.baseurl + subpath
 
-    def get(self, subpath, params):
+    def get(self, subpath, params, return_json=True):
         path = self._get_path(subpath)
+        resp = requests.get(path, params)
 
-    def post(self, subpath, params):
+        if return_json:
+            return resp.json()
+        else:
+            return resp.text()
+
+    def post(self, subpath, params, sign=True):
         """
         params: dict of the data
         """
         path = self._get_path(subpath)
-        sign = build_sign(params, self.secret_key)
-        params['sign'] = sign
-        resp = requests.post(path, json=params)
+
+        if sign:
+            sign = build_sign(params, self.secret_key)
+            params['sign'] = sign
+
+        resp = requests.post(path, params)
         return resp.json()
 
 class OKCoinClient:
@@ -49,8 +58,31 @@ class OKCoinClient:
         self.api = OKCoinReq(pkey, skey)
         self.pubilic_key = pkey
         self.secret_key = skey
-        
+
+
     def get_account_info(self):
         path = 'userinfo.do'
         resp = self.api.post(path, {'api_key': self.pubilic_key})
+
         return resp
+
+    def get_depth(self, pair):
+        """
+        pair: e.g. btc_cny, ltc_cny
+        """
+        path = 'depth.do'
+        return self.api.get(path, {'symbol': pair})
+
+    def trade(self, pair, price, amount, action):
+        """
+        api_key: apikey
+        symbol: e.g. ltc_cny
+        price: price
+        amount: amount
+        type: buy, sell, buy_market, sell_market
+        """
+        path = 'trade.do'
+        params = {'api_key': self.pubilic_key, 'symbol': pair,
+                  'price': price, 'amount': amount, 'type': action}
+
+        return self.api.post(path, params)
